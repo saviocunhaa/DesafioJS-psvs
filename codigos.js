115812bd4493661de69f0b22f0a35349a4de7f3c
saviocunhaa <saviocunha.01@hotmail.com>
2023-05-18 15:45:21 -0300

    desafio JS

---
+++ codigos.js
@@ -0,0 +1,17 @@
+
+
+function ativarNome(){
+    let nome = 'Sávio';
+    console.log(nome)
+}
+
+
+function somarNumeros(){
+    let a = 5;
+    let b = 10;
+
+
+    let c = a*b;
+
+    console.log(c)
+}
